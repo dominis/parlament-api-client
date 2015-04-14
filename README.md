0aad00ba9aadb705cde2f751a42646de5ee487a2
Nandor Sivok <dominis@haxor.hu>
2015-04-14 12:04:24 +0200

    added caching and logging
    - added logger for debug logging
    - support for redis cache
    - support for custom cache objects

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ You can obtain an access token at: http://www.parlament.hu/w-api-tajekoztato
 ## Getting started
 pip -r install requirements.txt
 
-## Usage:
+## Usage
+### Simple example
 ```python
 from papi import PAPI
 
@@ -18,3 +19,28 @@ print kepviselo['nev']
 for item in kepviselo['kepvcsop-tagsagok']['tagsag']:
      print '%s -> %s' % (item['@ciklus'], item['@kepvcsop'])
 ```
+
+### Debug mode
+```python
+from papi import PAPI
+
+c = PAPI(token='YOUR-TOKEN', debug=True)
+
+kepviselo = c.kepviselo(p_azon='z004')
+```
+
+### Redis cache
+```bash
+sudo pip install redis
+```
+
+```python
+import papi
+
+c = papi.PAPI(token='YOUR-TOKEN', debug=True, cache=papi.RedisCache())
+
+for i in range(0,100):
+    c.kepviselo(p_azon='z004')
+```
+
+
--- papi.py
+++ papi.py
@@ -3,6 +3,8 @@
 import requests
 import xmltodict
 from functools import partial, update_wrapper
+import logging
+
 
 class PAPI():
     endpoints = (
@@ -16,14 +18,32 @@ class PAPI():
 
     url_template = 'http://parlament.hu/cgi-bin/web-api/%s.cgi'
 
-    def __init__(self, token, *args, **kwargs):
+    def __init__(self, token, cache=None, debug=None):
         self.token = token
+        self.logger = logging.getLogger(__name__)
+
+        log_level = logging.INFO
+        if debug is not None:
+            log_level = logging.DEBUG
+
+        self.logger.setLevel(log_level)
+        handler = logging.StreamHandler()
+        handler.setLevel(log_level)
+
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+        assert isinstance(cache, (Cache, type(None))), \
+            "cache should be an instance of papi.cache"
+
+        self.cache = cache
 
     def __getattr__(self, key):
         self.endpoint = key
         if key in self.endpoints:
             url = self.url_template % key
-            mfun = getattr(self, 'client')
+            mfun = getattr(self, 'request_wrapper')
             fun = partial(mfun, url=url)
             return update_wrapper(fun, mfun)
         else:
@@ -32,16 +52,65 @@ class PAPI():
 
     __getitem__ = __getattr__
 
-    def client(self, url, **params):
+    def request_wrapper(self, url, **params):
+        self.logger.debug('processing request - url: %s params: %s' % (url, params))
         params.update({'access_token': self.token})
+
+        if self.cache:
+            key = self.cache.generateKey([url, params])
+            try:
+                self.logger.debug('cache get: %s' % key)
+                resp = self.cache.get(key)
+                self.logger.debug('cache hit: %s' % key)
+            except Exception:
+                self.logger.debug('cache miss: %s' % key)
+                resp = self.client(url, params)
+                try:
+                    self.cache.set(key, resp)
+                except Exception, e:
+                    self.logger.error('cache store failed - key: %s error: %s' % (key, e))
+
+        else:
+            resp = self.client(url, params)
+
+
+        return self.content(resp.text)
+
+    def client(self, url, params):
         r = requests.get(url, params=params)
         r.encoding = 'utf-8'
         if r.status_code != 200:
-            raise Exception(r.text.strip())
+            raise Exception('%s %s' % (url, r.text.strip()))
 
-        return self.content(r.text)
+        self.logger.debug('request: %s' % url)
+
+        return r
 
     def content(self, xml):
         return xmltodict.parse(xml)[self.endpoint]
 
 
+class Cache(object):
+    def __init__(self):
+        raise Exception (
+            'Please override __init__() to'
+            'provide a Connection object.'
+            )
+
+    def generateKey(*args):
+        import hashlib
+        import json
+
+        return hashlib.md5(json.dumps(args[1])).hexdigest()
+
+    def get(self, key):
+        return self.client.get(key)
+
+    def set(self, key, value):
+        return self.client.set(key, value)
+
+
+class RedisCache(Cache):
+    def __init__(self, host='localhost', port=6379, db=0):
+        import redis
+        self.client = redis.StrictRedis(host=host, port=port, db=db)
