5edeadf293e06d14aa93756b516ce648858d54a1
Nandor Sivok <dominis@haxor.hu>
2015-05-08 17:44:42 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='parlament-api-client',
+    version='0.0.1',
+    author='Nandor Sivok',
+    author_email='dominis@haxor.hu',
+    url='https://github.com/dominis/parlament-api-client',
+    description='A very simple python client for the Hungarian Parliament\'s api',
+)
